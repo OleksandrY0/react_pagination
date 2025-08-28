53d43b6d8db7c33b7b23d6c3c69efdefeddc3561
Oleksandr <sasha16092007@gmail.com>
2025-08-28 20:27:54 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -17,36 +17,6 @@ export const App: React.FC = () => {
   return (
     <div className="container">
       <h1>Items with Pagination</h1>
-
-      <p className="lead" data-cy="info">
-        Page 1 (items {startIndex + 1} - {Math.min(endIndex, items.length)} of
-        42)
-      </p>
-
-      <div className="form-group row">
-        <div className="col-3 col-sm-2 col-xl-1">
-          <select
-            data-cy="perPageSelector"
-            id="perPageSelector"
-            className="form-control"
-            value={perPage}
-            onChange={e => {
-              setPerPage(Number(e.target.value));
-              setCurrentPage(1);
-            }}
-          >
-            <option value="3">3</option>
-            <option value="5">5</option>
-            <option value="10">10</option>
-            <option value="20">20</option>
-          </select>
-        </div>
-
-        <label htmlFor="perPageSelector" className="col-form-label col">
-          items per page
-        </label>
-      </div>
-
       {/* Move this markup to Pagination */}
 
       <Pagination
@@ -54,6 +24,7 @@ export const App: React.FC = () => {
         perPage={perPage}
         currentPage={currentPage}
         onPageChange={setCurrentPage}
+        onPerPageChange={setPerPage}
       />
 
       <ul>
